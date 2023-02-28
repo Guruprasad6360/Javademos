febec15b4a5084414a6ba0e5f42c16023aab8300
Guruprasad6360 <96048149+Guruprasad6360@users.noreply.github.com>
2023-02-28 18:26:30 +0530

    Add files via upload
    Practice project

---
+++ java_first/src/Calculator.java
@@ -0,0 +1,31 @@
+package project_calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+ public static void main(String[] args) {
+  int n1,n2,oper;
+  System.out.println("1- Adding\n 2- Substraction\n 3- Division\n 4- Multiplication" );  // Asking input to the user..
+  System.out.println("Choose Operator:");
+  Scanner sc=new Scanner(System.in);
+  oper=sc.nextInt();
+  System.out.println("Enter first Number:");
+  n1=sc.nextInt();
+  System.out.println("Enter second Number:");
+  n2=sc.nextInt();
+  int result=0;
+  switch(oper) {
+       case 1: result=n1+n2;
+    		   break;
+       case 2: result=n1-n2;
+               break;
+       case 3: result=n1/n2;
+               break;
+       case 4: result=n1*n2;
+               break;
+       default : System.out.println("invalid operation");
+  }
+  System.out.println("RESULT:"+" "+result);
+  
+}
+}
